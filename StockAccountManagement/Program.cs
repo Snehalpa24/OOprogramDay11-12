ae266cf3834cc2c850811f62832f2582097887b6
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-30 17:40:39 +0530

    Stock Account Management program is added

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StockAccountManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FetchJson fetchJson = new FetchJson();
+            Stock data = fetchJson.Read("C:/Json/Stock.json");
+
+            Console.WriteLine(JsonConvert.SerializeObject(data));
+
+            Console.WriteLine("------------------------STOCK REPORT--------------------------");
+            for (int i = 0; i < data.Stock1.Count; i++)
+            {
+                Console.WriteLine("<-----------Details of Stock1-------------->");
+                Console.WriteLine("Share Name = " + data.Stock1[i].name);
+                Console.WriteLine("Share Price = " + data.Stock1[i].SharePrice);
+                Console.WriteLine("No Of Shares = " + data.Stock1[i].NoOfShares);
+                double Total = StockPortFolio.StockValue(data.Stock1[i].SharePrice, data.Stock1[i].NoOfShares);
+                Console.WriteLine("Value of Stock1 = " + Total);
+            }
+
+            Console.WriteLine("<-----------Details of Stock2-------------->");
+            for (int i = 0; i < data.Stock1.Count; i++)
+            {
+                Console.WriteLine("Share Name = " + data.Stock2[i].name);
+                Console.WriteLine("Share Price = " + data.Stock2[i].SharePrice);
+                Console.WriteLine("No Of Shares = " + data.Stock3[i].NoOfShares);
+                double Total = StockPortFolio.StockValue(data.Stock2[i].SharePrice, data.Stock2[i].NoOfShares);
+                Console.WriteLine("Value of Stock2 = " + Total);
+
+            }
+            Console.WriteLine("<-----------Details of Stock3-------------->");
+            for (int i = 0; i < data.Stock3.Count; i++)
+            {
+                Console.WriteLine("Share Name = " + data.Stock3[i].name);
+                Console.WriteLine("Share Price = " + data.Stock3[i].SharePrice);
+                Console.WriteLine("No Of Shares = " + data.Stock3[i].NoOfShares);
+                double Total = StockPortFolio.StockValue(data.Stock3[i].SharePrice, data.Stock3[i].NoOfShares);
+                Console.WriteLine("Value of Stock3 = " + Total);
+
+            }
+
+        }
+    }
+}
---
+++ StockAccountManagement/StockPortFolio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockAccountManagement
+{
+    class StockPortFolio
+    {
+        internal static double StockValue(double SharePrice, int NoOfShares)
+        {
+            double result = SharePrice * NoOfShares;
+            return result;
+        }
+    }
+}
